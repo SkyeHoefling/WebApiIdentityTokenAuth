ea87b57aac8b90ae50f3303398b66f3580ba6bce
Andrew Hoefling <andrew@hoeflingsoftware.com>
2017-04-18 01:35:29 -0400

    Configure identity database

--- Startup.cs
+++ Startup.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.EntityFrameworkCore;
 
 namespace WebApiIdentityTokenAuth
 {
@@ -27,8 +29,25 @@ namespace WebApiIdentityTokenAuth
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            // Add framework services.
+            // configure database
+            var server = "localhost"; 
+            var connection = $@"Server={server};Database=purchasing_dev;user=sa;password=Password01!;";
+            services.AddDbContext<IdentityDbContext>(options => options.UseSqlServer(
+                connection,
+                optionsBuilder => optionsBuilder.MigrationsAssembly("api")));
+
+            // configure token generation
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<IdentityDbContext>()
+                .AddDefaultTokenProviders();
+            
+            // configure identity options
+            services.Configure<IdentityOptions>(o => {
+                o.SignIn.RequireConfirmedEmail = true;
+            });
+
             services.AddMvc();
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
